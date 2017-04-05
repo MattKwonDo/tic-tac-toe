dc31900f9a11d4c0822a66f35f6177cb69637dfb
MattKwonDo <mgduvall@gmail.com>
2017-04-05 15:18:01 -0400

    add clickhandler, vars, start logic, update style
    Logic is still work in progress

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -46,8 +46,10 @@ const byId = function (e) {
 const onClick = function (event) {
   console.log('ayo222')
   // console.log($('#test1'))
-  $(this).css('background-color', 'black')
-  $(this).html('img src', 'images/questionBlock128.png')
+  $(this).css('background-color', '#5e91fe')
+  // $(this).css('border-color', '#5e91fe')
+  // $(this).html('img', src='images/questionBlock128.png')
+
   // console.log(document.getElementById($('#test1')))
   // return document.getElementById($('#test1')).src
   // = "images/questionBlock128.png"
@@ -58,15 +60,24 @@ const onClick = function (event) {
 }
 
 const addHandlers = () => {
-  $('#test1').on('click', onClick)
-  $('#test2').on('click', onClick)
-  $('#test3').on('click', onClick)
-  $('#test4').on('click', onClick)
-  $('#test5').on('click', onClick)
-  $('#test6').on('click', onClick)
-  $('#test7').on('click', onClick)
-  $('#test8').on('click', onClick)
-  $('#test9').on('click', onClick)
+  // $('#test1').on('click', onClick)
+  // $('#test2').on('click', onClick)
+  // $('#test3').on('click', onClick)
+  // $('#test4').on('click', onClick)
+  // $('#test5').on('click', onClick)
+  // $('#test6').on('click', onClick)
+  // $('#test7').on('click', onClick)
+  // $('#test8').on('click', onClick)
+  // $('#test9').on('click', onClick)
+  $('#test11').on('click', onClick)
+  $('#test12').on('click', onClick)
+  $('#test13').on('click', onClick)
+  $('#test14').on('click', onClick)
+  $('#test15').on('click', onClick)
+  $('#test16').on('click', onClick)
+  $('#test17').on('click', onClick)
+  $('#test18').on('click', onClick)
+  $('#test19').on('click', onClick)
 }
 
 module.exports = {
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -19,3 +19,131 @@ const authEvents = require('./auth/events.js')
 $(() => {
   authEvents.addHandlers()
 })
+
+let playerX = {
+  id: 1,
+  email: 'matt@m.com',
+  games_won: 0,
+  games_lost: 0,
+  games_drawn: 0
+  // ,   game_history: [] // push id, outcome
+}
+let playerO = {
+  id: 2,
+  email: 'fake@m.com',
+  games_won: 0,
+  games_lost: 0,
+  games_drawn: 0
+  // ,   game_history: [] // push id, outcome
+}
+let currentGame = {
+  winner: '',
+  loser: '',
+  drawer: '',
+  // only send below to API
+  id: 1,
+  cells: [], // should this be a 9 by 2 or 9 by 1 or 9 by 3
+  over: false,
+  player_x: {
+    id: playerX.id,
+    email: playerX.email
+  },
+  player_o: {
+    id: playerO.id,
+    email: playerO.email
+  }
+}
+
+// below is what needs to be sent to API
+let games = [
+  {
+      id: 1,
+      cells: ['o','x','o','x','o','x','o','x','o'],
+      over: true,
+      player_x: {
+        id: 1,
+        email: 'and@and.com'
+      },
+      player_o: {
+        id: 3,
+        email: 'dna@dna.com'
+      }
+    }
+]
+
+// define number of plays and playerTurn to keep track of who should be playing
+let plays = 0
+let playerTurn = 'playerX'
+
+// define grid to store game state
+let grid = [
+  [1,null,null],
+  [null,null,null],
+  [null,null,null]
+]
+let int = [[0,0,0],[null,1,null],[1,null,1]]
+
+let diagonalSum = function (array) {
+    let total = 0
+    for (row = 0; row < array.length; row++) {
+        total += array[row][row];
+    }
+    return total;
+}
+
+console.log(diagonalSum(int))
+
+// function to check to see if someone has won and change game states and winner loser, drawer, and counts
+// let overCheck = function {
+//   for (let i = 0; )
+//   let sumRow = grid[0].reduce((a, b) => a + b, 0);
+//   console.log(sum)
+//   let sumRow2 = grid[1].reduce((a, b) => a + b, 0);
+//   console.log(sumRow2)
+//   let sumRow3 = grid[1].reduce((a, b) => a + b, 0);
+//   console.log(sumRow2)
+// }
+
+
+let logic = function () {
+  // when click happens add 1 to plays and change playerTurn
+  // onClick - how to invoke???
+  plays += 1
+  if (playerTurn = 'playerX') {
+    playerTurn = 'playerO'
+  } else {
+    continue
+  }
+  // when plays < 5, continue, when greater or equal to 5 check to see if someone has won
+  if (plays < 4) {
+    continue
+  }
+  if (plays > 4) {
+
+  }
+  // change game status to over when 9 plays
+  if (plays = 9) {
+
+     if
+    currentGame.over = true
+  }
+
+  }
+
+
+// Game play:
+// 1. X starts
+// 2. X clicks a box
+//     1. If box has already been clicked, no change
+//     2. 1 is logged into an array representing that position
+//     3. Box changes color and displays X.
+// 3. O clicks a box
+//     1. If box has already been clicked, no change
+//     2. 0 is logged into an array representing that position
+//     3. Else box changes color and displays O
+// 4. 2 and 3 repeat loop until 3 in a row
+//     1. If count of row or column or diagonal values in array = 3
+//     2. AND (SUM = 3 or 0)
+//     3. THEN game over
+//     4. If SUM = 3, X wins
+//     5. If SUM = 0, O wins
